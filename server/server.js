bc8647009bc57b182ac55b2fc04b20fb4d7aab05
mohammad.a.aziz <mohammad.a.aziz@student.fh-kiel.de>
2017-01-06 09:48:13 +0100

    Added connect and disconnect client and server side

--- server/server.js
+++ server/server.js
@@ -1,14 +1,28 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 //console.log(publicPath);
 
 const port = process.env.PORT || 2000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, function(){
+//register an event e.g: connection
+io.on('connection', (socket) => {
+  console.log('Server saying: New user connected now');
+
+  socket.on('disconnect', () => {
+    console.log('Server saying: user disconnected now');
+  });
+})
+
+//app.listen(port, function(){
+server.listen(port, function(){
   console.log(`Server listening on port ${port}`);
 });
